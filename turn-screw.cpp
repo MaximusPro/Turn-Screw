3543bc866c289b0e4a1d508528f23850db5b9a0a
MaximusPro <maximusvj@gmail.com>
2024-10-13 08:30:58 +0200

    Update turn-screw.cpp
    Added new code and fixed bugs

--- turn-screw.cpp
+++ turn-screw.cpp
@@ -8,18 +8,50 @@ using namespace std::filesystem;
 // using::std::getline;
 
 #define version 0.1111
+#define ERROR_TPL -1
+#define ERROR_DIR -2
+#define ERROR_FILE -3
+#define ERROR_ARGS -4
+
+
 
 std::string TemplatesWords[]{
     "#FILENAME#",
     "#MAINFILECPP#"};
 
 std::string Commands[]{
+    "cconfig",
     "--man",
     "-tpl",
     "--version",
-    "-pp", // FullPathProject
+    "-pp", // FullPathToProject
     "-np", // NameProject
-    "-save-tpl"
+    "save",
+    "{CURRENT_DIRECTORY}",
+    "-lg"
+};
+
+std::string PreDefVariables[]{
+
+    "${userHome}", // - the path of the user's home folder
+    "${workspaceFolder}", // - the path of the folder opened in VS Code
+    "${workspaceFolderBasename}", // - the name of the folder opened in VS Code without any slashes (/)
+    "${file}",// - the current opened file
+    "${fileWorkspaceFolder}", // - the current opened file's workspace folder
+    "${relativeFile}", // - the current opened file relative to workspaceFolder
+    "${relativeFileDirname}", // - the current opened file's dirname relative to workspaceFolder
+    "${fileBasename}", // - the current opened file's basename
+    "${fileBasenameNoExtension}", // - the current opened file's basename with no file extension
+    "${fileExtname}", // - the current opened file's extension
+    "${fileDirname}", // - the current opened file's folder path
+    "${fileDirnameBasename}", // - the current opened file's folder name
+    "${cwd}", // - the task runner's current working directory upon the startup of VS Code
+    "${lineNumber}", // - the current selected line number in the active file
+    "${selectedText}", // - the current selected text in the active file
+    "${execPath}",// - the path to the running VS Code executable
+    "${defaultBuildTask}", // - the name of the default build task
+    "${pathSeparator}", // - the character used by the operating system to separate components in file paths
+    "${/}" // - shorthand for ${pathSeparator}
 
 };
 
@@ -28,7 +60,7 @@ int manual()
     std::ifstream File("/usr/share/turn-screw/manual.txt");
     if (!File.is_open())
     {
-        std::cout << "Could not open manual!\n"
+        std::cout << "ERROR: Could not open manual!\n"
                   << std::endl;
         return 1;
     }
@@ -80,6 +112,8 @@ struct Arguments
     std::string tpl;
     std::string NameProject;
     std::string FullPathProject;
+    bool CreateConfigFiles;
+    bool SaveTPL;
     // std::string man;
 };
 
@@ -111,6 +145,207 @@ std::string ChangeLine(std::string &Line, Arguments &MainArgs, std::pair<int, in
     return Str;
 }
 
+int CreateConfigFiles(Arguments& Args)
+{
+    if (!Args.tpl.empty() && Args.CreateConfigFiles == true)
+    {
+        path CurrPath = current_path();
+        std::cout << CurrPath << std::endl;
+        std::string StrPathToTemplates = (char *)CurrPath.c_str();
+        StrPathToTemplates += "/.templates";
+        path PathToTemplates(StrPathToTemplates);
+        std::string DirTemplates(Args.tpl);
+        DirTemplates += ".tpl";
+        if (exists(PathToTemplates))
+        {
+            std::string StrPathToDirTemplate;
+            StrPathToDirTemplate = PathToTemplates.c_str();
+            StrPathToDirTemplate += '/';
+            StrPathToDirTemplate += DirTemplates;
+            path PathToDirTemplate(StrPathToDirTemplate);
+            if (!exists(PathToDirTemplate))
+            {
+                std::cout << "ERROR: Directory of templates:" << StrPathToDirTemplate << " is not exist!\n";
+                return ERROR_TPL;
+            }
+            std::cout << "It's work" << std::endl;
+            std::string LaunchFile = "launch.json";
+            std::string TasksFile = "tasks.json";
+            std::string PathToLuanchFile = StrPathToDirTemplate + '/' + LaunchFile;
+            std::ifstream InFileLaunch;
+            std::ofstream OutFileLaunch;
+            std::string FullPathProjectPlusDirVSCode;
+            if (Args.FullPathProject.empty() || Args.FullPathProject == "{CURRENT_DIRECTORY}")
+            {
+                FullPathProjectPlusDirVSCode = CurrPath.c_str();
+                if(!Args.NameProject.empty())
+                {
+                    FullPathProjectPlusDirVSCode += "/";
+                    FullPathProjectPlusDirVSCode += Args.NameProject;
+                }
+            }
+            else
+                FullPathProjectPlusDirVSCode = Args.FullPathProject;
+            FullPathProjectPlusDirVSCode += "/.vscode";
+            path OutPathProjectJsonFiles(FullPathProjectPlusDirVSCode);
+            if (!exists(OutPathProjectJsonFiles))
+            {
+                create_directory(OutPathProjectJsonFiles);
+                if (exists(OutPathProjectJsonFiles))
+                    std::cout << "Created directory: " << OutPathProjectJsonFiles << std::endl;
+            }
+            std::string FullPathToLaunchFile = FullPathProjectPlusDirVSCode;
+            FullPathToLaunchFile += "/";
+            FullPathToLaunchFile += LaunchFile;
+            OutFileLaunch.open(FullPathToLaunchFile, std::ofstream::out);
+            if (!OutFileLaunch.is_open())
+            {
+                std::cout << "ERROR: File: " << LaunchFile << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+
+            InFileLaunch.open(PathToLuanchFile);
+            if (!InFileLaunch.is_open())
+            {
+                std::cout << "ERROR: File: " << LaunchFile << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+            std::string Line;
+            while (getline(InFileLaunch, Line))
+            {
+                // std::cout << Line << std::endl;
+                std::pair<int, int> IndexsWords = FindTemplateWord(Line);
+                if (IndexsWords.first != 0 && IndexsWords.second != 0)
+                {
+                    std::string ConstWord;
+                    for (int i = IndexsWords.first; i < IndexsWords.second + 1; i++)
+                        ConstWord += Line[i];
+                    std::string StringLine = ChangeLine(Line, Args, IndexsWords);
+                    OutFileLaunch << StringLine << std::endl;
+                    // std::cout << StringLine << std::endl;
+                    // std::cout << ConstWord << std::endl;
+                }
+                else
+                {
+                    OutFileLaunch << Line << std::endl;
+                }
+            }
+            InFileLaunch.close();
+            OutFileLaunch.close();
+            std::string FullPathToTasksFile = FullPathProjectPlusDirVSCode;
+            FullPathToTasksFile += "/";
+            FullPathToTasksFile += TasksFile;
+            std::string StrPathToTasksFile = StrPathToDirTemplate + '/' + TasksFile;
+            std::ofstream OutTasksFile;
+            std::ifstream InTasksFile;
+            InTasksFile.open(StrPathToTasksFile);
+            OutTasksFile.open(FullPathToTasksFile, std::ofstream::out);
+            if (!InTasksFile.is_open())
+            {
+                std::cout << "ERROR: File: " << TasksFile << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+            if (!OutTasksFile.is_open())
+            {
+                std::cout << "ERROR: File: " << TasksFile << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+            while (getline(InTasksFile, Line))
+            {
+                // std::cout << Line << std::endl;
+                std::pair<int, int> IndexsWords = FindTemplateWord(Line);
+                if (IndexsWords.first != 0 && IndexsWords.second != 0)
+                {
+                    std::string ConstWord;
+                    for (int i = IndexsWords.first; i < IndexsWords.second + 1; i++)
+                        ConstWord += Line[i];
+                    std::string StringLine = ChangeLine(Line, Args, IndexsWords);
+                    OutTasksFile << StringLine << std::endl;
+                    std::cout << StringLine << std::endl;
+                    // std::cout << ConstWord << std::endl;
+                }
+                else
+                {
+                    OutTasksFile << Line << std::endl;
+                }
+            }
+            InTasksFile.close();
+            OutTasksFile.close();
+        }
+    }
+    else {
+        std::cout << "ERROR: Argument -tpl is empty!" << std::endl;
+        return ERROR_ARGS;
+    }
+    return 0;
+}
+
+int SaveTemplates(Arguments& Args)
+{
+    path PathToProject;
+    if(Args.SaveTPL == true)
+    {
+        std::cout << "SaveMode..." << std::endl;
+        if(Args.FullPathProject.empty() && Args.NameProject.empty() && Args.tpl.empty())
+        {
+            return ERROR_ARGS;
+        }
+        if(!Args.FullPathProject.empty())
+        {
+            PathToProject = Args.FullPathProject;
+            if(exists(PathToProject))
+            {
+                std::cout << "It's path is fine!" << std::endl;
+            }
+        }
+        else {
+            std::cout << "Argument -pp is empty! Use current path for save template!" << std::endl;
+            PathToProject = current_path();
+            std::cout << "Current path: " << PathToProject.c_str() << std::endl;
+        }
+        if(!Args.NameProject.empty())
+        {
+            std::string ChangePath = PathToProject.c_str();
+            ChangePath += "/";
+            ChangePath += Args.NameProject;
+            ChangePath += "/";
+            ChangePath += ".vscode";
+            PathToProject = ChangePath;
+            if(exists(PathToProject))
+            std::cout << "Directory \".vscode\" is found!" << std::endl;
+            else{return ERROR_DIR;}
+            std::string FullPathToLaunchFile = ChangePath;
+            std::string FullPathToTasksFile = ChangePath;
+            FullPathToLaunchFile += "/launch.json";
+            FullPathToTasksFile += "/tasks.json";
+            std::ifstream InFileLaunch(FullPathToLaunchFile);
+            if (!InFileLaunch.is_open())
+            {
+                std::cout << "ERROR: File: " << "launch.json" << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+            std::vector<std::string> StringsLaunchFile;
+            std::string Line;
+            while(getline(InFileLaunch, Line))
+            {
+                StringsLaunchFile.push_back(Line);
+            }
+            std::ifstream InFileTasks(FullPathToTasksFile);
+            if (!InFileTasks.is_open())
+            {
+                std::cout << "ERROR: File: " << "tasks.json" << " is not opened!" << std::endl;
+                return ERROR_FILE;
+            }
+            std::vector<std::string> StringsTasksFile;
+            while(getline(InFileTasks, Line))
+            {StringsTasksFile.push_back(Line);}
+            
+            
+        }
+    }
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     // for(std::string S:TemplatesWords) std::cout << S << std::endl;
@@ -133,7 +368,7 @@ int main(int argc, char *argv[])
     for (int i = 0; i != argc; i++)
     {
         StrArg1 = argv[i];
-        if (IsCommand(StrArg1))
+        if (IsCommand(StrArg1) && argc != i+1)
             if (StrArg1 == "-tpl")
             {
                 StrArg2 = argv[i + 1];
@@ -154,147 +389,47 @@ int main(int argc, char *argv[])
             }
             else if (StrArg1 == "--man")
             {
+                if(argc<3)
                 return manual();
+                else
+                {
+                    std::cout << "ERROR: You have mistakes in arguments!" << std::endl;
+                    return ERROR_ARGS;
+                }
             }
             else if (StrArg1 == "--version")
             {
                 std::cout << "version: " << version << std::endl;
                 return 0;
             }
+            else if(StrArg1 == "cconfig")
+            {
+                MainArguments.CreateConfigFiles = true;
+            }
+            else if(StrArg1 == "save")
+            MainArguments.SaveTPL = true;
     }
-    if (argc == 2 || argc % 2 == 0)
+    if (argc == 2 || (MainArguments.CreateConfigFiles != true && MainArguments.SaveTPL != true))
     {
         std::cout << "ERROR: You have mistakes in arguments!" << std::endl;
-        return 2;
+        return ERROR_ARGS;
     }
+    int NONERRORS = NULL;
     // std::cout << "Hello!\n";
-    if (!MainArguments.tpl.empty())
-    {
-        path CurrPath = current_path();
-        std::cout << CurrPath << std::endl;
-        std::string StrPathToTemplates = (char *)CurrPath.c_str();
-        StrPathToTemplates += "/.templates";
-        path PathToTemplates(StrPathToTemplates);
-        std::string DirTemplates(MainArguments.tpl);
-        DirTemplates += ".tpl";
-        if (exists(PathToTemplates))
-        {
-            std::string StrPathToDirTemplate;
-            StrPathToDirTemplate = PathToTemplates.c_str();
-            StrPathToDirTemplate += '/';
-            StrPathToDirTemplate += DirTemplates;
-            path PathToDirTemplate(StrPathToDirTemplate);
-            if (!exists(PathToDirTemplate))
-            {
-                std::cout << "Directory of templates:" << StrPathToDirTemplate << " is not exist!\n";
-                return -1;
-            }
-            std::cout << "It's work" << std::endl;
-            std::string LaunchFile = "launch.json";
-            std::string TasksFile = "tasks.json";
-            std::string PathToLuanchFile = StrPathToDirTemplate + '/' + LaunchFile;
-            std::ifstream InFileLaunch;
-            std::ofstream OutFileLaunch;
-            std::string FullPathProjectPlusDirVSCode;
-            if (MainArguments.FullPathProject.empty())
-            {
-                FullPathProjectPlusDirVSCode = CurrPath.c_str();
-            }
-            else
-                FullPathProjectPlusDirVSCode = MainArguments.FullPathProject;
-            FullPathProjectPlusDirVSCode += "/.vscode";
-            path OutPathProjectJsonFiles(FullPathProjectPlusDirVSCode);
-            if (!exists(OutPathProjectJsonFiles))
-            {
-                create_directory(OutPathProjectJsonFiles);
-                if (exists(OutPathProjectJsonFiles))
-                    std::cout << "Created directory: " << OutPathProjectJsonFiles << std::endl;
-            }
-            std::string FullPathToLaunchFile = FullPathProjectPlusDirVSCode;
-            FullPathToLaunchFile += "/";
-            FullPathToLaunchFile += LaunchFile;
-            OutFileLaunch.open(FullPathToLaunchFile, std::ofstream::out);
-            if (!OutFileLaunch.is_open())
-            {
-                std::cout << "File: " << LaunchFile << " is not opened!" << std::endl;
-                return -1;
-            }
-
-            InFileLaunch.open(PathToLuanchFile);
-            if (!InFileLaunch.is_open())
-            {
-                std::cout << "File: " << LaunchFile << " is not opened!" << std::endl;
-                return -1;
-            }
-            std::string Line;
-            while (getline(InFileLaunch, Line))
-            {
-                // std::cout << Line << std::endl;
-                std::pair<int, int> IndexsWords = FindTemplateWord(Line);
-                if (IndexsWords.first != 0 && IndexsWords.second != 0)
-                {
-                    std::string ConstWord;
-                    for (int i = IndexsWords.first; i < IndexsWords.second + 1; i++)
-                        ConstWord += Line[i];
-                    std::string StringLine = ChangeLine(Line, MainArguments, IndexsWords);
-                    OutFileLaunch << StringLine << std::endl;
-                    // std::cout << StringLine << std::endl;
-                    // std::cout << ConstWord << std::endl;
-                }
-                else
-                {
-                    OutFileLaunch << Line << std::endl;
-                }
-            }
-            InFileLaunch.close();
-            OutFileLaunch.close();
-            std::string FullPathToTasksFile = FullPathProjectPlusDirVSCode;
-            FullPathToTasksFile += "/";
-            FullPathToTasksFile += TasksFile;
-            std::string StrPathToTasksFile = StrPathToDirTemplate + '/' + TasksFile;
-            std::ofstream OutTasksFile;
-            std::ifstream InTasksFile;
-            InTasksFile.open(StrPathToTasksFile);
-            OutTasksFile.open(FullPathToTasksFile, std::ofstream::out);
-            if (!InTasksFile.is_open())
-            {
-                std::cout << "File: " << TasksFile << " is not opened!" << std::endl;
-                return -1;
-            }
-            if (!OutTasksFile.is_open())
-            {
-                std::cout << "File: " << TasksFile << " is not opened!" << std::endl;
-                return -1;
-            }
-            while (getline(InTasksFile, Line))
-            {
-                // std::cout << Line << std::endl;
-                std::pair<int, int> IndexsWords = FindTemplateWord(Line);
-                if (IndexsWords.first != 0 && IndexsWords.second != 0)
-                {
-                    std::string ConstWord;
-                    for (int i = IndexsWords.first; i < IndexsWords.second + 1; i++)
-                        ConstWord += Line[i];
-                    std::string StringLine = ChangeLine(Line, MainArguments, IndexsWords);
-                    OutTasksFile << StringLine << std::endl;
-                    std::cout << StringLine << std::endl;
-                    // std::cout << ConstWord << std::endl;
-                }
-                else
-                {
-                    OutTasksFile << Line << std::endl;
-                }
-            }
-            InTasksFile.close();
-            OutTasksFile.close();
-        }
+    if(MainArguments.CreateConfigFiles == true)
+    NONERRORS = CreateConfigFiles(MainArguments);
+    if(NONERRORS == 0 && MainArguments.CreateConfigFiles == true){
+        //if(MainArguments.FullPathProject.empty() || MainArguments.NameProject.empty() || MainArguments.tpl.empty())
+        std::cout << "CreateConfigFiles..." << std::endl;
     }
-
-    else
-    {
-        std::cout << "Directory with templates is not exist!\n";
-        return -1;
+    /*else if(NONERRORS == ERROR_TPL)
+    {    
+        std::cout << "ERROR: Directory with templates is not exist!\n";
+        return ERROR_TPL;
     }
-
+    */
+   NONERRORS = SaveTemplates(MainArguments);
+    if(NONERRORS == 0 && MainArguments.SaveTPL == true)
+    {}
     return 0;
 }
